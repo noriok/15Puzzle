c6fb2664bd0e61a9d1bcc1113dec80645abeedb7
Norio Kobayashi <noriokob@gmail.com>
2016-01-22 06:23:51 +0900

    update

---
+++ Assets/Scripts/MainSystem.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System;
+
+public class MainSystem : MonoBehaviour {
+    private const float PIECE_SIZE = 1.35f;
+    private const int N = 4;
+    private GameObject[,] _grid = new GameObject[N, N];
+
+
+    private Vector3 GetPiecePosition(int row, int col) {
+        var x = PIECE_SIZE * col;
+        var y = -PIECE_SIZE * row;
+        return new Vector3(x, y, 0);
+    }
+
+	// Use this for initialization
+	void Start () {
+        // float size = PIECE_SIZE;
+        for (int i = 1; i <= 15; i++) {
+            var prefabPath = "Prefabs/piece" + i;
+
+            int row = (i - 1) / 4;
+            int col = (i - 1) % 4;
+            // var x = size * col;
+            // var y = -size * row;
+            var pos = GetPiecePosition(row, col);
+            var prefab = Resources.Load(prefabPath);
+            var g = (GameObject)Instantiate(prefab, pos, Quaternion.identity);
+            // g.GetComponent<Renderer>().enabled = false;
+            g.GetComponent<PieceBase>().Id = i;
+
+            _grid[row, col] = g;
+        }
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D c = Physics2D.OverlapPoint(pos);
+            if (c != null) {
+                // Debug.Log("clicked: " + c);
+
+                int id = c.gameObject.GetComponent<PieceBase>().Id;
+                Debug.Log("clicked piece id: " + id);
+                MovePiece(id);
+            }
+        }
+	}
+
+    private void MovePiece(int id) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (_grid[i, j] == null) continue;
+                if (id != _grid[i, j].GetComponent<PieceBase>().Id) continue;
+
+                var dxy = new[] { 0, 1, 0, -1 };
+                for (int k = 0; k < dxy.Length; k++) {
+                    int r = i + dxy[k];
+                    int c = j + dxy[3-k];
+                    if (0 <= r && r < N && 0 <= c && c < N) {
+                        if (_grid[r, c] == null) {
+                            // swap (i, j) -- (r, c)
+                            Debug.Log(string.Format("swap ({0}, {1}) - ({2}, {3})", i, j, r, c));
+                            _grid[r, c] = _grid[i, j];
+                            _grid[i, j] = null;
+
+                            _grid[r, c].transform.position = GetPiecePosition(r, c);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        Debug.Log("移動できません: id:" + id);
+    }
+}
---
+++ Assets/Scripts/PieceBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceBase : MonoBehaviour {
+    public int Id { get; set; }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+/*
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("hoge");
+        if (eventData.clickCount == 1) {
+            Debug.Log("クリックされました");
+        }
+    }
+*/
+}
