39aa6841a624f297375e8545b67aeeb5afb6355b
Norio Kobayashi <noriokob@gmail.com>
2016-02-09 01:39:37 +0900

    Add Shuffle Animation

--- Assets/Scripts/MainSystem.cs
+++ Assets/Scripts/MainSystem.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
 
 public class MainSystem : MonoBehaviour {
     private const float PIECE_SIZE = 1.35f;
     private const int N = 4;
     private GameObject[,] _grid = new GameObject[N, N];
+
+    private bool _isAnimationRunning = false;
 
     private Vector3 GetPiecePosition(int row, int col) {
         var x = PIECE_SIZE * col;
@@ -27,17 +32,162 @@ public class MainSystem : MonoBehaviour {
 	}
 
 	void Update () {
+        // アニメーション中なら入力操作は受け付けない
+        if (_isAnimationRunning) return;
+
         if (Input.GetMouseButtonDown(0)) {
             var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Collider2D c = Physics2D.OverlapPoint(pos);
             if (c != null) {
-                int id = c.gameObject.GetComponent<PieceBase>().Id;
+                // int id = c.gameObject.GetComponent<PieceBase>().Id;
                 // Debug.Log("clicked piece id: " + id);
-                MovePiece(id);
+
+                int row = -1, col = -1;
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < N; j++) {
+                        if (c.gameObject == _grid[i, j]) {
+                            row = i;
+                            col = j;
+                            break;
+                        }
+                    }
+                }
+                Assert.IsTrue(row != -1 && col != -1);
+                StartCoroutine(Slide(row, col));
             }
         }
 	}
 
+    void OnGUI() {
+        Func<string, bool> button = (caption) => {
+            return GUILayout.Button(caption, GUILayout.Width(110), GUILayout.Height(50));
+        };
+
+        if (GUILayout.Button("Shuffle 10 times", GUILayout.Width(110), GUILayout.Height(50))) {
+            if (_isAnimationRunning) return;
+            StartCoroutine(Shuffle(10));
+        }
+        else if (GUILayout.Button("Shuffle 30 times", GUILayout.Width(110), GUILayout.Height(50))) {
+            if (_isAnimationRunning) return;
+            StartCoroutine(Shuffle(30));
+        }
+        else if (button("Shuffle 50 times")) {
+            if (_isAnimationRunning) return;
+            StartCoroutine(Shuffle(50));
+        }
+        else if (button("Shuffle Cancel")) {
+        }
+    }
+
+    private void Swap<T>(ref T a, ref T b) {
+        var t = a; a = b; b = t;
+    }
+
+    // (row, col) のピースをスライドさせる
+    IEnumerator Slide(int row, int col) {
+        if (_grid[row, col] == null) {
+            Debug.Log(string.Format("ピースがありません。row:{0} col:{1}", row, col));
+            yield break;
+        }
+
+        // 空きピースの位置
+        var pos = GetEmptyPieceLoc();
+        if (!IsNeighbor(row, col, pos[0], pos[1])) {
+            Debug.Log(string.Format("空きセルが隣接していません。row:{0} col:{1}", row, col));
+            yield break;
+        }
+
+        _isAnimationRunning = true;
+        int dstRow = row + (pos[0] - row);
+        int dstCol = col + (pos[1] - col);
+        var target = _grid[row, col];
+        var src = GetPiecePosition(row, col);
+        var dst = GetPiecePosition(dstRow, dstCol);
+        float duration = 0.2f;
+        float elapsed = 0;
+        while (elapsed <= duration) {
+            float x = Mathf.Lerp(src.x, dst.x, elapsed / duration);
+            float y = Mathf.Lerp(src.y, dst.y, elapsed / duration);
+            target.transform.position = new Vector3(x, y, 0);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        Swap(ref _grid[row, col], ref _grid[dstRow, dstCol]);
+
+        // 各ピースの位置を揃える(ずれ補正)
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (_grid[i, j] == null) continue;
+                _grid[i, j].transform.position = GetPiecePosition(i, j);
+            }
+        }
+        _isAnimationRunning = false;
+    }
+
+    IEnumerator Shuffle(int count) {
+        var delta = new List<int[]>() {
+            new[] { -1, 0 },
+            new[] { 1, 0 },
+            new[] { 0, -1 },
+            new[] { 0, 1},
+        };
+
+        var rand = new System.Random();
+        int drow = 0;
+        int dcol = 0;
+
+        int cnt = 0;
+        while (cnt < count) {
+            var pos = GetEmptyPieceLoc();
+
+            int p = rand.Next(delta.Count);
+            // 前の状態に後戻りさせない
+            if (drow == -delta[p][0] && dcol == -delta[p][1]) continue;
+
+            int r = pos[0] + delta[p][0];
+            int c = pos[1] + delta[p][1];
+            if (0 <= r && r < N && 0 <= c && c < N) {
+                drow = delta[p][0];
+                dcol = delta[p][1];
+                yield return StartCoroutine(Slide(r, c));
+                cnt++;
+            }
+        }
+    }
+
+    // 上下左右で隣接しているか
+    private bool IsNeighbor(int r1, int c1, int r2, int c2) {
+        int dr = Math.Abs(r1 - r2);
+        int dc = Math.Abs(c1 - c2);
+        return dr + dc == 1;
+    }
+
+    private int[] GetEmptyPieceLoc() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (_grid[i, j] == null) {
+                    return new[] { i,  j };
+                }
+            }
+        }
+        Assert.IsTrue(false, "空きピースがありません");
+        return null;
+    }
+
+    private GameObject GetPiece(int id) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (_grid[i, j] == null) continue;
+                if (id == _grid[i, j].GetComponent<PieceBase>().Id) {
+                    return _grid[i, j];
+                }
+            }
+        }
+        Assert.IsTrue(false, "不正なIDです: " + id);
+        return null;
+    }
+
+/*
     private void MovePiece(int id) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -64,4 +214,5 @@ public class MainSystem : MonoBehaviour {
         }
         Debug.Log("移動できません: id:" + id);
     }
+*/
 }
